9c433621598f64103dfb8328c4af38db1ebe21b6
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2021-02-13 23:19:00 +0300

    alt/seruman: use RGB_MATRIX_CUSTOM_off at startup

--- keyboards/massdrop/alt/keymaps/seruman/config.h
+++ keyboards/massdrop/alt/keymaps/seruman/config.h
@@ -1,5 +1,3 @@
 #pragma once
 
-// Disable RGB by default
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
-#define RGB_MATRIX_STARTUP_VAL 0
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_off
